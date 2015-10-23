1880a30f5d89b6d5bbaa646f1e8b7fa93a62d049
kolbeypruitt <kolbeypruitt@gmail.com>
2015-10-22 18:23:18 -0600

    initial commit, oauth2_google functioning

---
+++ app.js
@@ -0,0 +1,128 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var passport = require('passport')
+var session = require('cookie-session')
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+require('dotenv').load();
+var db = require('monk')(process.env.MONGOLAB_URI || 'localhost/trunk_swap');
+var trunkdb = db.get('trunk');
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(session({
+  name: 'session',
+  keys: [
+  'key'
+  ]
+}));
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://127.0.0.1:3000/oauth2callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's Google profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Google account with a user record in your database,
+      // and return that user instead.
+      // console.log(profile);
+      return done(null, profile);
+    });
+  }
+));
+// right above app.use('/', routes);
+app.use(function (req, res, next) {
+  // console.log(res.locals);
+  res.locals.user = req.user
+  next()
+})
+
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: 'https://www.googleapis.com/auth/plus.login' }));
+
+app.get('/oauth2callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.render('success', { userName: profile.displayName })
+  });
+
+app.get('/logout', function(req, res){
+  req.session = null;
+  res.redirect('/')
+});
+
+app.use('/', routes);
+app.use('/users', users);
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var db = require('monk')(process.env.MONGOLAB_URI || 'localhost/trunk_swap');
+var trunkdb = db.get('trunk');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: 'We failed....' });
+});
+
+
+module.exports = router;
