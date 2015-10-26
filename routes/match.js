54456c60f4951886e5fa77d580e385822e52b896
kolbeypruitt <kolbeypruitt@gmail.com>
2015-10-26 00:37:03 -0600

    i don't know why my findTrunkMatch method doesn't work

--- routes/match.js
+++ routes/match.js
@@ -9,13 +9,18 @@ var TrunkLib = require('../lib/mongo')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if(req.user) {
-    console.log(req.user);
-    console.log(req.body);
+    // console.log(req.user);
+    // console.log(req.body);
     usersdb.findOne({_id: req.user._id}).then(function (user) {
       return trunkdb.find({_id: user.trunk_id});
-    }).then(function (trunk) {
+    }).then(function (myTrunk) {
       // this should find the my users trunk offer
       // next we need to run the searchForTrunk method in lib folder
+        TrunkLib.findTrunkMatch(myTrunk).then(function (matches) {
+        console.log(matches);
+        res.render('match', {title: 'Trunk Man', displayName: req.user.displayName, matches: matches })
+      })
+      
     })
 
 
