431ea02e4b267c78e1fc7f541224b88ee9caba24
kolbeypruitt <kolbeypruitt@gmail.com>
2015-10-25 21:41:41 -0600

    search results page needs work but is functionalish

--- routes/search/index.js
+++ routes/search/index.js
@@ -11,43 +11,25 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  var trunkParams = req.body;
-  var user = req.session.passport.user;
-  // TrunkLib.searchForTrunk(trunkParams, user).then(function (searchResults) {
-  //   res.render('search/results', { title: 'Post Offer', allTrunks: searchResults });
-  // })
-    console.log(req.body);
-    var obj = {}
-    if(req.body.findYear!='null') {
-        obj['current_year'] = req.body.findYear;
-    }
-    if(req.body.findModel!='null') {
-        obj['current_model'] = req.body.findModel;
-    }
-    if(req.body.findStyle!='null') {
-        obj['current_style'] = req.body.findStyle;
-    }
-
-    trunkdb.find(obj, function(err, docs){
-        if (err) return err;
-        // console.log(docs);
-        // res.send(docs);
-    }).then(function (docs) {
-      res.render('search/results', { title: 'Post Offer', allTrunks: docs });
-    })
-    
-
-  // return trunkdb.find({
-  //   "$text": {
-  //     "$search": req.body.query
-  //   }}).then(function (searchResults) {
-  //     console.log(searchResults);
-  //     res.render('search/results', { title: 'Post Offer', allTrunks: searchResults });
-  //   })
+  console.log(req.body);
+  var obj = {}
+  if(req.body.findYear!='null') {
+      obj['current_year'] = req.body.findYear;
+  }
+  if(req.body.findModel!='null') {
+      obj['current_model'] = req.body.findModel;
+  }
+  if(req.body.findStyle!='null') {
+      obj['current_style'] = req.body.findStyle;
+  }
+  trunkdb.find(obj, function(err, docs){
+      if (err) return err;
+      // console.log(docs);
+      // res.send(docs);
+  }).then(function (docs) {
+    res.render('search/results', { title: 'Post Offer', displayName:req.user.displayName, allTrunks: docs });
+  })
 });
 
-router.get('/results', function(req, res, next) {
-  res.render('search/results', { title: 'Post Offer', results: records });
-});
 
 module.exports = router;
