a7b10062565085161047332eb15239b1a8083129
kolbeypruitt <kolbeypruitt@gmail.com>
2015-10-27 09:24:51 -0600

    match page now is semi-functional

--- routes/match.js
+++ routes/match.js
@@ -8,24 +8,23 @@ var dbLib = require('../lib/mongo')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // dbLib.findMyMatches(req.user._id)
   if(req.user) {
-    // console.log(req.user);
-    // console.log(req.body);
     usersdb.findOne({_id: req.user._id}).then(function (user) {
-      return trunkdb.find({_id: user.trunk_id});
-    }).then(function (myTrunk) {
-      // this should find the my users trunk offer
-      // next we need to run the searchForTrunk method in lib folder
-        dbLib.findTrunkMatch(myTrunk).then(function (matches) {
-        console.log(matches);
-        res.render('match', {title: 'Trunk Man', displayName: req.user.displayName, matches: matches })
+      return trunkdb.findOne({_id: user.trunk_id});
+    }).then(function (myOffer) {
+      var array = [];
+      array.push({ 'current_year': myOffer.desired_year })
+      array.push({ 'current_model': myOffer.desired_model })
+      array.push({ 'current_style': myOffer.desired_style })
+      return trunkdb.find( { $and: [ array[0], array[1], array[2] ] }, function (err, allMatches) {
+        res.render('match', {title: 'Trunk Man', allMatches: allMatches})
       })
-      
     })
 
-
-    res.render('match', {title: 'Trunk Man', displayName: req.user.displayName })
-  } else {
+    
+  } 
+  else {
     res.render('index', {title: 'Trunk Man'})
   }
 });
