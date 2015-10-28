9fd97d9585dc5c4660d716b8b17ca484773dc59c
kolbeypruitt <kolbeypruitt@gmail.com>
2015-10-28 14:59:22 -0600

    moved browse backend code to lib file and fixed counter

--- routes/browse.js
+++ routes/browse.js
@@ -6,40 +6,9 @@ var usersdb = db.get('users');
 var dbLib = require('../lib/mongo')
 
 router.get('/', function (req, res, next) {
-  var records = {};
-  trunkdb.find({}).then(function (allTrunks) {
-    return allTrunks;
-  }).then(function (allTrunks) {
-    records.allTrunks = allTrunks;
-    return usersdb.find({}).then(function (allUsers) {
-      records.allUsers = allUsers;
-      return records;
-    }).then(function (records) {
-      // this code is incomplete and needs to build a new object to send to view
-      // we also need to make a new route to display users/trunks to satisfy
-      // assessment requirements
-      var allTrunks = records.allTrunks;
-      var allUsers = records.allUsers;
-      var viewObj = {};
-      for (var i = 0; i < allTrunks.length; i++) {
-        var trunk = allTrunks[i];
-        // finding correct trunk for user
-        var user = dbLib.findUserOfTrunk(trunk, allUsers)
-        viewObj['trunk' + '_' + i] = { _id: trunk._id
-                                      ,current_year: trunk.current_year
-                                      ,current_model: trunk.current_model
-                                      ,current_style: trunk.current_style
-                                      ,firstName: user.firstName
-                                      ,lastName: user.lastName
-                                      ,email: user.email
-                                      ,phone: user.phone
-                                      ,location: 'locations pending'
-                                    };
-      }
-      console.log(viewObj);
-      res.render('browse', {allTrunks: viewObj, displayName: req.user.displayName})
-    });
-  });
+  dbLib.modelDisplayObjTrunksAndUsers().then(function (viewObj) {
+    res.render('browse', {allTrunks: viewObj, displayName: req.user.displayName})
+  })
 });
 
 module.exports = router;
