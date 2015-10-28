be38ddac6ff33a4cc97ab0bfee1b33a97db0d628
kolbeypruitt <kolbeypruitt@gmail.com>
2015-10-28 11:55:36 -0600

    started working on showing user who posted trunk in browse page

--- routes/browse.js
+++ routes/browse.js
@@ -6,8 +6,25 @@ var usersdb = db.get('users');
 var dbLib = require('../lib/mongo')
 
 router.get('/', function (req, res, next) {
-  trunkdb.find({}).then(function (records) {
-    res.render('browse', {allTrunks: records, displayName: req.user.displayName})
+  var records = {};
+  trunkdb.find({}).then(function (allTrunks) {
+    return allTrunks;
+  }).then(function (allTrunks) {
+    records.allTrunks = allTrunks;
+    return usersdb.find({}).then(function (allUsers) {
+      records.allUsers = allUsers;
+      return records;
+    }).then(function (records) {
+      // this code is incomplete and needs to build a new object to send to view
+      // we also need to make a new route to display users/trunks to satisfy
+      // assessment requirements
+      var allTrunks = records.allTrunks;
+      var allUsers = records.allUsers;
+      for (var i = 0; i < allTrunks.length; i++) {
+        allTrunks[i].user_id
+      }
+      res.render('browse', {allTrunks: allTrunks, allUsers: allUsers, displayName: req.user.displayName})
+    });
   });
 });
 
