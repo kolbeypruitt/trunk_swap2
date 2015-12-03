6fce0b39bfec9f6703a045f5a907f2c330a8a16c
kolbeypruitt <kolbeypruitt@gmail.com>
2015-12-03 12:55:08 -0700

    deleted trunkman extra folder

--- routes/browse.js
+++ routes/browse.js
@@ -9,6 +9,15 @@ router.get('/', function (req, res, next) {
   dbLib.modelDisplayObjTrunksAndUsers().then(function (viewObj) {
     res.render('browse', {allTrunks: viewObj, displayName: req.user.displayName})
   })
+
+// 
+// 
+// 
+// 
+// 
+// 
+
+  
 });
 
 module.exports = router;
